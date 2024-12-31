db84c310d89f47acf7a358bf1f76e216495091b2
Md Moniruzzaman <web.moniruzzaman1@gmail.com>
2024-12-31 21:58:15 +0600

    create patient controlelr done

---
+++ src/app/modules/user/user.contollers.ts
@@ -0,0 +1,20 @@
+import catchAsync from "../../utils/catchAsync";
+import sendResponse from "../../utils/sendResponse";
+import { userServices } from "./user.services";
+
+
+
+const createPatient = catchAsync(async (req, res) => {
+    const { password, patient: patientData } = req.body
+    const result = await userServices.createPatientIntoDB(password, patientData)
+    sendResponse(res, {
+        statusCode: 201,
+        success: true,
+        message: 'Patient create successfully',
+        data: result
+    })
+})
+
+export const userController = {
+    createPatient
+}
