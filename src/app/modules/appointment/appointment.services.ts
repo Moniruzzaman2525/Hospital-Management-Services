3c4e2bfea93e4ad696bd7febe1fb4f96b79e7491
Md Moniruzzaman <web.moniruzzaman1@gmail.com>
2025-01-03 14:41:16 +0600

    update

--- src/app/modules/appointment/appointment.services.ts
+++ src/app/modules/appointment/appointment.services.ts
@@ -4,3 +4,8 @@ import { Appointment } from "./appointment.model"
 const bookAppointmentServices = async () => {
     const result = await Appointment.create()
 }
+
+
+export const appointmentServices = {
+    bookAppointmentServices
+}
