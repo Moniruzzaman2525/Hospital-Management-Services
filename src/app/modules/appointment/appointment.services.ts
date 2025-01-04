547111ba60901c131f67505ae031e7227092361e
Md Moniruzzaman <web.moniruzzaman1@gmail.com>
2025-01-04 15:35:35 +0600

    update

--- src/app/modules/appointment/appointment.services.ts
+++ src/app/modules/appointment/appointment.services.ts
@@ -1,3 +1,4 @@
+import AppError from "../../errors/AppError"
 import { Doctor } from "../doctor/doctor.model"
 import { TAppointment } from "./appointment.interface"
 import { Appointment } from "./appointment.model"
@@ -10,6 +11,9 @@ const bookAppointmentServices = async (payload: TAppointment) => {
 
     const appointmentId = await generateAppointmentId()
     const isDoctorExist = await Doctor.findOne({ id: payload.id })
+    if (!isDoctorExist) {
+        throw new AppError(404, 'Doctor not found')
+    }
     payload.id = appointmentId
     const result = await Appointment.create(payload)
     return result
