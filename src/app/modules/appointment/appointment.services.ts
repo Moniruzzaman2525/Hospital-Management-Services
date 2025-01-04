c5a16ff6c4c6616f8198661da8fd295f0251ae10
Md Moniruzzaman <web.moniruzzaman1@gmail.com>
2025-01-04 15:34:23 +0600

    update

--- src/app/modules/appointment/appointment.services.ts
+++ src/app/modules/appointment/appointment.services.ts
@@ -1,3 +1,4 @@
+import { Doctor } from "../doctor/doctor.model"
 import { TAppointment } from "./appointment.interface"
 import { Appointment } from "./appointment.model"
 import { generateAppointmentId } from "./appointment.utils"
@@ -5,9 +6,10 @@ import { generateAppointmentId } from "./appointment.utils"
 
 const bookAppointmentServices = async (payload: TAppointment) => {
 
-    
+
 
     const appointmentId = await generateAppointmentId()
+    const isDoctorExist = await Doctor.findOne({ id: payload.id })
     payload.id = appointmentId
     const result = await Appointment.create(payload)
     return result
